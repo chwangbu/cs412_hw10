1e2ce8e7a51fd73b8c9d007d1257d1e5fa291f36
Chan Hwang <bryho@bu.edu>
2024-11-13 19:00:46 -0500

    Circular import, moved load_data function into utils.py

--- voter_analytics/models.py
+++ voter_analytics/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-import csv
-from datetime import datetime
-from .models import Voter
 
 # Create your models here.
 class Voter(models.Model):
@@ -24,26 +21,3 @@ class Voter(models.Model):
 
     def __str__(self):
         return f"{self.firstName} {self.lastName} - Precinct {self.precinctNumber}"
-
-def load_data(file_path = '/Users/chwang/Downloads/newton_voters.csv'):
-    with open(file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            Voter.objects.create(
-                lastName = row['Last Name'],
-                firstName = row['First Name'],
-                streetNumber = row["Residential Address - Street Numner"],
-                streetName = row['Residential Address -  Street Name'],
-                apartmentNumebr = row.get('Residential Address - Apartment Number', None),
-                zipCode = row['Residential Address - Zip Code'],
-                dateOfBirth=datetime.strptime(row['Date of Birth'], '%Y-%m-%d').date(),
-                dateOfRegistration=datetime.strptime(row['Date of Registration'], '%Y-%m-%d').date(),
-                partyAffiliation=row['Party Affiliation'],
-                precinctNumber=int(row['Precinct Number']),
-                v20State=row['v20state'] == 'TRUE',
-                v21Town=row['v21town'] == 'TRUE',
-                v21Primary=row['v21primary'] == 'TRUE',
-                v22General=row['v22general'] == 'TRUE',
-                v23Town=row['v23town'] == 'TRUE',
-                voterScore=int(row['voter_score']),
-            )
---
+++ voter_analytics/utils.py
@@ -0,0 +1,26 @@
+import csv
+from datetime import datetime
+from .models import Voter
+
+def load_data(file_path = '/Users/chwang/Downloads/newton_voters.csv'):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Voter.objects.create(
+                lastName = row['Last Name'],
+                firstName = row['First Name'],
+                streetNumber = row["Residential Address - Street Numner"],
+                streetName = row['Residential Address -  Street Name'],
+                apartmentNumebr = row.get('Residential Address - Apartment Number', None),
+                zipCode = row['Residential Address - Zip Code'],
+                dateOfBirth=datetime.strptime(row['Date of Birth'], '%Y-%m-%d').date(),
+                dateOfRegistration=datetime.strptime(row['Date of Registration'], '%Y-%m-%d').date(),
+                partyAffiliation=row['Party Affiliation'],
+                precinctNumber=int(row['Precinct Number']),
+                v20State=row['v20state'] == 'TRUE',
+                v21Town=row['v21town'] == 'TRUE',
+                v21Primary=row['v21primary'] == 'TRUE',
+                v22General=row['v22general'] == 'TRUE',
+                v23Town=row['v23town'] == 'TRUE',
+                voterScore=int(row['voter_score']),
+            )
