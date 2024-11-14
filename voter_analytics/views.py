64edecac347d1a622f98452e0cb52003ce76d7f8
Chan Hwang <bryho@bu.edu>
2024-11-13 19:31:14 -0500

    DefinedVoterListView

--- voter_analytics/views.py
+++ voter_analytics/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Voter
 
 # Create your views here.
+class VoterListView(ListView):
+    model = Voter
+    template_name = 'voter_analytics/voter_list.html'
+    context_object_name = 'voters'
+    paginate_by = 100
